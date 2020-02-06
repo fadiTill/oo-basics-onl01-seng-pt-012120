7de097427881cb5cc54a75319780e5f9263a2f1a
fadimata tillman <fadi.tillman@gmail.com>
2020-02-06 20:17:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,18 @@
 # Make your shoe class here!def initialize(title)
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
+#   def initialize (brand)
+#     @brand = brand
+# end
+# def cobble
+# if   condition = Shoe.new("new")
+#      condition = new
+#     puts  "Your shoe is as good as new!"
+#   end
+# end
+# end 
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -6,9 +20,7 @@ class Shoe
     @brand = brand
 end
 def cobble
-if   condition = Shoe.new("new")
-     condition = new
-    puts  "Your shoe is as good as new!"
+    puts "Your shoe is as good as new!"
+    condition = "new"
   end
 end
-end 
